313df5ff1eeaaeb7d3461f99df4ad6f265025faa
Florsalcedowd <florenciasalcedo13@gmail.com>
2019-05-13 00:13:48 -0300

    Ejercicio 1

---
+++ Ej_Debug0/src/ej_debug0/Main.java
@@ -0,0 +1,30 @@
+
+
+package ej_debug0;
+
+class B {
+    public int acumular() {
+        int acu =0;
+        
+        for(int i=-1000; i<1000; i++) {
+            acu += i/2;
+        }
+        return acu;
+    }
+}
+class A {
+    
+    public void callB() {
+       B b1 = new B();   
+       System.out.println( b1.acumular() );
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+        a.callB();
+    }
+
+}
